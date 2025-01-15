4e25f81025de3fc341bc13cdfac0b45e96cdafca
Habiba Halkano <Habzhalka@gmail.com>
2025-01-15 11:06:20 +0300

    modifying plpweek4.py

--- plpweek4.py
+++ plpweek4.py
@@ -1,9 +1,11 @@
+#part 1 of the assignment
 file = open("data.txt", "r")
 print(file.read())
 
 newfile = open("data1.txt", "w")
 newfile.write("this is now a modified file, notice the change")
 
+#part 2 of the assignment
 file = input("Enter file name:")
 try:
     file = open(file)
